63bfe3404d1aff7393c35e533776453369b0cec8
Joseph Aboagye <josephkwabenaaboagye@gmail.com>
2021-09-13 21:02:01 +0000

    Checks if a singly linked list is a palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,22 +1,57 @@
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+int check_size(listint_t *head);
 
 /**
- * is_palindrome - Checks if a number is a palindrome
- * @n: The given unsigned integer
+ * is_palindrome - check if the linked list is a palindrome
+ * @head: list to be checked
  *
- * Return: Either 1 if n is a palindrome or 0 if not
+ * Return: 0 if it isnt and 1 if it is
  */
 int is_palindrome(listint_t **head)
 {
-  int remain, num, sum = 0;
+	listint_t *temp = *head;
+	int size = check_size(temp), i = 0;
+	int check_A[12700];
 
-  for (num = n; n != 0; n = n / 10)
-    {
-      remain = n % 10;
-      sum = sum * 10 + remain;
-    }
-  if (num == sum)
-    return (1);
-  else
-    return (0);
+	if (check_A == NULL)
+		return (0);
+	if (size == 0)
+	{
+		return (1);
+	}
+	while (temp != NULL)
+	{
+		check_A[i] = temp->n;
+		temp = temp->next;
+		i++;
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (check_A[i] != check_A[(size - 1) - i])
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
+
+/**
+ * check_size - return size of list
+ * @head: list to be checked
+ *
+ * Return: size of list
+ */
+
+int check_size(listint_t *head)
+{
+	int i = 0;
+
+	while (head != NULL)
+	{
+		i++;
+		head = head->next;
+	}
+	return (i);
 }
