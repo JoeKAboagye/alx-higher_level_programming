04a8037b4dad0cfdcb52b4ea4278b9d2314c20cb
JoeKAboagye <Josephkwabenaaboagye@gmail.com>
2021-12-19 16:43:50 -0800

    Lists states of a DB

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,15 +6,10 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
     cursor = db.cursor()
-    state_name = sys.argv[4]
-    cursor.execute("SELECT * FROM states WHERE CAST(name AS BINARY) LIKE " +
-            "CAST('{}' AS BINARY) ORDER BY id ASC;".format(state_name)
+    cursor.execute("SELECT * FROM states WHERE name = '{}'".format(argv[4]))
     for data in cursor.fetchall():
         print(data)
     cursor.close()
