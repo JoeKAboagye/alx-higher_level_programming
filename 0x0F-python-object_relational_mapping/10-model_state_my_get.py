2966bfa7edc1c1b87c92114188e8a3e4fbe01289
JoeKAboagye <Josephkwabenaaboagye@gmail.com>
2021-12-19 17:39:11 -0800

    List states object of DB

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Prints the id of a State object with a given name in a database"""
-
+'''Prints the id of a State object with a given name in a database.
+'''
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -10,15 +10,15 @@ from model_state import Base, State
 
 if __name__ == '__main__':
     if len(sys.argv) >= 5:
-        """ username = sys.argv[1]
-        password = sys.argv[2]
-        database name = sys.argv[3] """
+        user = sys.argv[1]
+        pword = sys.argv[2]
+        db_name = sys.argv[3]
         state_name = sys.argv[4]
-        check = map(lambda x: x.isalpha() or (x in (' ', '%', '_')), state_name)
-        if not all(check):
+        chk = map(lambda x: x.isalpha() or (x in (' ', '%', '_')), state_name)
+        if not all(chk):
             state_name = ''
         DATABASE_URL = "mysql://{}:{}@localhost:3306/{}".format(
-            sys.argv[1], sys.argv[2], sys.argv[3]
+            user, pword, db_name
         )
         engine = create_engine(DATABASE_URL)
         Base.metadata.create_all(engine)
