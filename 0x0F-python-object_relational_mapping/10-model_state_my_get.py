13a6aab3fd2bfa78e64edad519eb49272d5374ac
JoeKAboagye <Josephkwabenaaboagye@gmail.com>
2021-12-19 17:34:55 -0800

    List states object of DB

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Prints the id of a State object with a given name in a database"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from model_state import Base, State
+
+
+if __name__ == '__main__':
+    if len(sys.argv) >= 5:
+        """ username = sys.argv[1]
+        password = sys.argv[2]
+        database name = sys.argv[3] """
+        state_name = sys.argv[4]
+        check = map(lambda x: x.isalpha() or (x in (' ', '%', '_')), state_name)
+        if not all(check):
+            state_name = ''
+        DATABASE_URL = "mysql://{}:{}@localhost:3306/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        )
+        engine = create_engine(DATABASE_URL)
+        Base.metadata.create_all(engine)
+        session = sessionmaker(bind=engine)()
+        result = session.query(State).filter(State.name == state_name).first()
+        if result is not None:
+            print('{}'.format(result.id))
+        else:
+            print('Not found')
