fe636a8ec36ae1525797c1b01767721049fdad63
JoeKAboagye <Josephkwabenaaboagye@gmail.com>
2021-12-19 17:27:37 -0800

    List states object of DB

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
-"""
-Prints all State objects with a name that contains 'a' in a database
-"""
-from sys import argv
+'''Prints all State objects with a name that contains 'a' in a database.
+'''
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 from model_state import Base, State
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import Session
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-                           format(argv[1], argv[2], argv[3]),
-                           pool_pre_ping=True)
-    Base.metadata.create_all(engine)
 
-    session = Session(engine)
-    data = session.query(State).order_by(State.id)\
-                               .filter(State.name.like("%a%")).all()
-    for row in data:
-        print("{}: {}".format(row.id, row.name))
-    session.close()
+if __name__ == '__main__':
+    if len(sys.argv) >= 4:
+        user = sys.argv[1]
+        pword = sys.argv[2]
+        db_name = sys.argv[3]
+        DATABASE_URL = "mysql://{}:{}@localhost:3306/{}".format(
+            user, pword, db_name
+        )
+        engine = create_engine(DATABASE_URL)
+        Base.metadata.create_all(engine)
+        session = sessionmaker(bind=engine)()
+        result = session.query(State).order_by(State.id.asc()).filter(
+            State.name.like('%a%')
+        )
+        for data in datault:
+            print('{}: {}'.format(data.id, data.name))
