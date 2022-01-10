34b64cea32d3f9b6122f7bc724fc3e91f437fc23
JoeKAboagye <Josephkwabenaaboagye@gmail.com>
2022-01-09 19:04:04 -0800

    Python backened

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python3
 """lists the most recent 10 commits from a passed repository/owner"""
-from sys import argv
-from requests import get
+import requests
+import sys
 
 
 if __name__ == "__main__":
-    url = "https://api.github.com/repos"
-    url += "/{}/{}/commits".format(argv[2], argv[1])
-    request = get(url)
-    commits = request.json()
-    try:
-        for i in range(10):
-            print(commits.get('sha'), end=': ')
-            print(commits.get('commit').get('author').get('name'))
-    except IndexError:
-        pass
+    if len(sys.argv) > 2:
+        repository_name = sys.argv[1]
+        owner_name = sys.argv[2]
+        api_url = 'https://api.github.com'
+        req_url = '{}/repos/{}/{}/commits?{}'.format(
+            api_url,
+            owner_name,
+            repository_name,
+            'per_page=10'
+        )
+        response = requests.get(
+            req_url,
+            headers={'Accept': 'application/vnd.github.v3+json'}
+        )
+        if response.status_code == 200:
+            for commit in response.json():
+                commit_sha = commit['sha']
+                commit_author = commit['commit']['author']['name']
+                print('{}: {}'.format(commit_sha, commit_author))
